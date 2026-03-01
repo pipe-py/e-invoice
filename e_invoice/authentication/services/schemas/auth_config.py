fe1612ed739b1b595777302d9a98ff6aada6244f
pipe <pipe@felipeoidor.com>
2026-03-01 00:44:21 -0500

    refactor(auth): reorganize authentication app structure

---
+++ e_invoice/authentication/services/schemas/auth_config.py
@@ -0,0 +1,13 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class OAuth2Credentials(BaseSettings):
+    user_name: SecretStr
+    password: SecretStr
+    client_id: SecretStr
+    client_secret: SecretStr
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="UTF-8"
+    )
