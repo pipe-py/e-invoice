1a20eb1237600eac117b6ce0c2b18f5e41e90028
pipe <pipe@felipeoidor.com>
2026-03-08 19:41:49 -0500

    feat(form): add basic invoice form

--- e_invoice/factus_api/form.py
+++ e_invoice/factus_api/form.py
@@ -2,3 +2,11 @@ from django import forms
 from core.utils.constants import CODE_TABLES
 
 
+class InvoiceForm(forms.Form):
+    # Customer
+    numbering_range_id = forms.ChoiceField(label="Rango de numeración", choices=())
+    reference_code = forms.CharField(label="Código de referencia")
+    client = forms.ChoiceField(label="Cliente", choices=())
+    product = forms.ChoiceField(label="Producto", choices=())
+    payment_method = forms.ChoiceField(label="Método de pago", choices=())
+    observation = forms.CharField(label="Observaciones", widget=forms.Textarea)
