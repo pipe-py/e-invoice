f0f55c2ef6fc8af22c322ebcc64c79851d5ae2ef
pipe <pipe@felipeoidor.com>
2026-03-09 21:55:06 -0500

    feat(models): add customer model

--- e_invoice/factus_api/models.py
+++ e_invoice/factus_api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    identification = models.CharField(
+        max_length=20, unique=True, verbose_name="Identificación"
+    )
+    dv = models.PositiveSmallIntegerField(
+        null=True, blank=True, verbose_name="Dígito de verificación"
+    )
+    company = models.CharField(
+        max_length=100, null=True, blank=True, verbose_name="Razón Social"
+    )
+    trade_name = models.CharField(
+        max_length=100, null=True, blank=True, verbose_name="Nombre Comercial"
+    )
+    names = models.CharField(
+        max_length=100, null=True, blank=True, verbose_name="Nombre"
+    )
+    address = models.CharField(
+        max_length=200, null=True, blank=True, verbose_name="Dirección"
+    )
+    email = models.EmailField(
+        max_length=100, null=True, blank=True, verbose_name="email"
+    )
+    phone = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name="Teléfono"
+    )
+    legal_organization_id = models.PositiveIntegerField(
+        verbose_name="Tipo de Organización Legal"
+    )
+    tribute_id = models.PositiveIntegerField(verbose_name="Responsabilidad Tributaria")
+    identification_document_id = models.PositiveIntegerField(
+        verbose_name="Tipo de Documento"
+    )
+    municipality_id = models.PositiveIntegerField(
+        null=True, blank=True, verbose_name="Municipio"
+    )
