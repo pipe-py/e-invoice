948e93d517c0e90ac7bceaa1373be04671df12d4
pipe <pipe@felipeoidor.com>
2026-03-11 08:01:43 -0500

    feat(form) add ModelChoiceField for client and product in invoiceform

--- e_invoice/factus_api/form.py
+++ e_invoice/factus_api/form.py
@@ -1,12 +1,21 @@
 from django import forms
 from core.utils.constants import CODE_TABLES
+from factus_api.models import Customer, Items
 
 
 class InvoiceForm(forms.Form):
     # Customer
     numbering_range_id = forms.ChoiceField(label="Rango de numeración", choices=())
     reference_code = forms.CharField(label="Código de referencia")
-    client = forms.ChoiceField(label="Cliente", choices=())
-    product = forms.ChoiceField(label="Producto", choices=())
+    client = forms.ModelChoiceField(
+        label="Cliente",
+        queryset=Customer.objects.all(),
+        empty_label="Seleccione un Cliente",
+    )
+    product = forms.ModelChoiceField(
+        label="Producto",
+        queryset=Items.objects.all(),
+        empty_label="Seleccione un Producto",
+    )
     payment_method = forms.ChoiceField(label="Método de pago", choices=())
     observation = forms.CharField(label="Observaciones", widget=forms.Textarea)
