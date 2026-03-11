0bc0b9646cff6718a9054154debe58ce28aced37
pipe <pipe@felipeoidor.com>
2026-03-10 23:14:13 -0500

    feat(models): add establishment model

--- e_invoice/factus_api/models.py
+++ e_invoice/factus_api/models.py
@@ -49,3 +49,14 @@ class Customer(models.Model):
             return f"{self.names} - {self.identification}"
 
         return str(self.identification)
+
+
+class Establishment(models.Model):
+    name = models.CharField(verbose_name="Nombre",max_length=200)
+    address = models.CharField(verbose_name="Dirección",max_length=200)
+    phone_number = models.CharField(verbose_name="Teléfono", max_length=20)
+    email = models.EmailField(verbose_name="Email", max_length=200)
+    municipality_id = models.PositiveIntegerField(verbose_name="Municipio")
+
+    def __str__(self):
+        return self.name
